2ef447c3ce5c1f655088855d5bb1cfbcb5926c55
ykamila <ykamila23@gmail.com>
2023-09-24 17:06:45 +0200

    created packages, started with Event and Graph classes

---
+++ src/main/kotlin/de/unisaarland/cs/se/selab/events/Events.kt
@@ -0,0 +1,20 @@
+package de.unisaarland.cs.se.selab.events
+import de.unisaarland.cs.se.selab.graph.PrimaryType
+
+abstract class Event(open val eventId: Int, val duration: Int, val tick: Int) {
+}
+
+// Subclass for Vehicle Unavailable event
+class VehicleUnavailable(val vehicleId: Int, override val eventId:Int, duration: Int, tick: Int) : Event(eventId, duration, tick)
+
+// Subclass for Road Closure event
+class RoadClosure(val sourceID: Int, val targetID: Int, override val eventId: Int, duration: Int, tick: Int) : Event(eventId, duration, tick)
+
+// Subclass for Rush Hour event
+class RushHour(val roadType: PrimaryType, override val eventId:Int, duration: Int, tick: Int) : Event(eventId, duration, tick)
+
+// Subclass for Traffic Jam event
+class TrafficJam(val factor: Int, val sourceId: Int, val targetId: Int,override val eventId:Int, duration: Int, tick: Int) : Event(eventId, duration, tick)
+
+// Subclass for Construction event
+class Construction(val factor: Int, val streetClosed: Boolean, val sourceId: Int, val targetId: Int,override val eventId:Int, duration: Int, tick: Int) : Event(eventId, duration, tick)
---
+++ src/main/kotlin/de/unisaarland/cs/se/selab/graph/Graph.kt
@@ -0,0 +1,9 @@
+package de.unisaarland.cs.se.selab.graph
+
+enum class PrimaryType{
+    MAIN_STREET,
+    SIDE_STREET,
+    COUNTY_ROAD
+}
+class Graph {
+}
