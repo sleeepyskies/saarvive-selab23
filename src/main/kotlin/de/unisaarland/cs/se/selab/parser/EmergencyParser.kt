948eac9b150d3b68f9fb35a2c550c9fd9dba3056
ykamila <ykamila23@gmail.com>
2023-09-25 12:52:00 +0200

    Started working on Emergency parser

--- src/main/kotlin/de/unisaarland/cs/se/selab/parser/EmergencyParser.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/parser/EmergencyParser.kt
@@ -1,28 +1,22 @@
 package de.unisaarland.cs.se.selab.parser
-
-import de.unisaarland.cs.se.selab.dataClasses.emergencies.Emergency
+import org.json.*
+import de.unisaarland.cs.se.selab.dataClasses.Emergency
+import de.unisaarland.cs.se.selab.getSchema
+import java.io.File
+import javax.swing.text.html.parser.Parser
 
 class EmergencyParser {
-    public fun parse(): Emergency? {
+
+    var emergencies = mutableListOf<Emergency>()
+    var schema = getSchema(Parser::class.java,"emergency.schema")
+    var json = JSONObject(File(schema).readText())
+
+    public fun parse(fileName: String): List<Emergency> {
+//        for (i in 0 <= until < json.getJSONARRAY of )
 //        return Emergency()
-        return null
+        return emptyList()
     }
 
-    private fun createBlueprint(): List<String> {
-        return listOf()
-    }
 
-    private fun validateBlueprint(blueprint: List<String>): Boolean {
-        return true
-    }
-
-    private fun createEmergency(emergency: String): Emergency? {
-        return null
-    }
-
-    private fun createEmergencyList(emergencies: List<String>): List<Emergency> {
-        val emergencies = mutableListOf<Emergency>()
-        return emergencies
-    }
 
 }
