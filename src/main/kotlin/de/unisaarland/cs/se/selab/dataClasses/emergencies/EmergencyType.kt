b1e9b28fb420462cd5194f6f97f7f686a1c3b807
Iryna Kmet <irkm00001@stud.uni-saarland.de>
2023-09-25 15:06:14 +0200

    Added documentation to EmergencyType

--- src/main/kotlin/de/unisaarland/cs/se/selab/dataClasses/emergencies/EmergencyType.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/dataClasses/emergencies/EmergencyType.kt
@@ -1,5 +1,8 @@
 package de.unisaarland.cs.se.selab.dataClasses.emergencies
 
+/**
+ * Enumeration class for all possible emergencies in the simulation
+ */
 enum class EmergencyType {
     FIRE,
     CRIME,
