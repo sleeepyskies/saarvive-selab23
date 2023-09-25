691dd10b793e52b96620a492be76d7a4aa57f009
skysw <skwa00001@stud.uni-saarland.de>
2023-09-25 11:28:57 +0200

    removed VehicleType duplicate

--- src/main/kotlin/de/unisaarland/cs/se/selab/dataClasses/Vehicle.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/dataClasses/Vehicle.kt
@@ -3,18 +3,6 @@ package de.unisaarland.cs.se.selab.dataClasses
 import de.unisaarland.cs.se.selab.graph.Road
 import de.unisaarland.cs.se.selab.graph.Vertex
 
-enum class VehicleType {
-    POLICE_CAR,
-    POLICE_MOTORCYCLE,
-    K9_POLICE_CAR,
-    FIRE_TRUCK_WATER,
-    FIRE_TRUCK_TECHNICAL,
-    FIRE_TRUCK_LADDER,
-    FIREFIGHTER_TRANSPORTER,
-    AMBULANCE,
-    EMERGENCY_DOCTOR_CAR
-}
-
 enum class VehicleStatus {
     RECHARGING,
     IN_BASE,
