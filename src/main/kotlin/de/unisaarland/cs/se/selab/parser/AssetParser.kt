73fb94117610e9fd5ffac93de42622bc46a76869
htutwin <miwi00005@stud.uni-saarland.de>
2023-09-25 15:08:12 +0200

    added parse in assetparser

--- src/main/kotlin/de/unisaarland/cs/se/selab/parser/AssetParser.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/parser/AssetParser.kt
@@ -2,13 +2,20 @@ package de.unisaarland.cs.se.selab.parser
 
 import de.unisaarland.cs.se.selab.dataClasses.Base
 import de.unisaarland.cs.se.selab.dataClasses.Vehicle
+import java.io.File
 
 class AssetParser {
-    public fun parseAsset():List<Base> {
-        return emptyList()
+    public fun parse(file: File): List<Base> {
+        val jsonObject = JSONObject(file.readText())
+        val blueprint = createBlueprint(jsonObject)
+        if (validateBlueprint(blueprint)) {
+            return createBaseList(blueprint)
+        } else {
+            throw IllegalArgumentException("blueprint is invalid")
+        }
     }
 
-    private fun createBluePrint(): Map <String, String> {
+    private fun createBlueprint(jsonObject: JSONObject): Map<String, String> {
         val blueprint = mutableMapOf<String, String>()
         return blueprint
     }
@@ -17,23 +24,22 @@ class AssetParser {
         return true
     }
 
-    private fun createVehicle (blueprint: Map<String, String>): Vehicle? {
+    private fun createVehicle(blueprint: Map<String, String>): Vehicle? {
         return null
     }
 
-    private fun createBase (blueprint: Map<String, String>, vehicles: List<Vehicle>): Base? {
+    private fun createBase(blueprint: Map<String, String>, vehicles: List<Vehicle>): Base? {
         return null
     }
 
-    private fun createBaseList (bases: List<Base>): List<Base> {
+    private fun createBaseList(blueprint: Map<String, String>): List<Base> {
         val bases = mutableListOf<Base>()
         return bases
     }
 
     // here it should take a blueprint as an argument, but it doesn't make sense
-    private fun createVehicleList (vehicles: List<Vehicle>): List<Vehicle> {
+    private fun createVehicleList(vehicles: List<Vehicle>): List<Vehicle> {
         val vehicles = mutableListOf<Vehicle>()
         return vehicles
     }
-
 }
