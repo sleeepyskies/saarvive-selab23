0fb6ccfea48f7d9824bbb821106832ecfbe38158
Umama <umri00001@stud.uni-saarland.de>
2023-09-25 16:49:15 +0200

    solved merge conflict

---
+++ src/main/kotlin/de/unisaarland/cs/se/selab/dataClasses/PoliceStation.kt
@@ -0,0 +1,5 @@
+package de.unisaarland.cs.se.selab.dataClasses
+
+
+class PoliceStation (private val baseID:Int, private  val staff : Int, private val vertexID : string, private val vehicles : List<Vehicle>, private val dogs : Int) : Base (baseID, staff, vertexID, vehicles){
+}
