ef9c488cf160af3464d94438b577a9ecbe63d0d5
Iryna Kmet <irkm00001@stud.uni-saarland.de>
2023-09-26 12:13:23 +0200

    County parser terminates if file not exists

--- src/main/kotlin/de/unisaarland/cs/se/selab/parser/CountyParser.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/parser/CountyParser.kt
@@ -3,22 +3,44 @@ package de.unisaarland.cs.se.selab.parser
 import de.unisaarland.cs.se.selab.graph.Graph
 import de.unisaarland.cs.se.selab.graph.Road
 import de.unisaarland.cs.se.selab.graph.Vertex
+import java.io.File
+import kotlin.system.exitProcess
 
-class CountyParser {
-//    public fun parse():Graph {
-//        return Graph()
-//    }
+/**
+ * Class for parsing the .dot file that contains information about Map. Receives [dotFilePath] as path to the .dot data
+ * file
+ */
+class CountyParser(private val dotFilePath: String) {
+    private val dotFile: File
+    private val data: String
+    private val roads: List<Road> = mutableListOf() // List of roads on the map (for compatability)
 
-    private fun createBlueprint(): Map<String, String>{
+    init {
+        try {
+            // Convert path to a file
+            this.dotFile = File(dotFilePath)
+        } catch (_: NullPointerException) {
+            // File doesn't exist
+            exitProcess(1)
+        }
+    }
+   fun parse():Graph {
+       // Creating it if the syntax is valid
+       val blueprint = createBlueprint()
+      return Graph()
+   }
+
+    private fun createBlueprint(): Map<String, String> {
         val blueprint = mutableMapOf<String, String>()
+this.
         return blueprint
     }
 
-    private fun validateBlueprint(blueprint: Map<String, String>):Boolean{
+    private fun validateBlueprint(blueprint: Map<String, String>): Boolean {
         return true
     }
 
-    private fun createGraph(vertices: List<Vertex>):Graph{
+    private fun createGraph(vertices: List<Vertex>): Graph {
         return Graph(vertices)
     }
 
@@ -40,7 +62,7 @@ class CountyParser {
 //        return Vertex()
 //    }
 
-    private fun connectVertices(vertices: List<Vertex>, roads: List<Road>, blueprint: Map<String, String>): Unit {
+    private fun connectVertices(vertices: List<Vertex>, roads: List<Road>, blueprint: Map<String, String>) {
         return Unit
     }
 }
