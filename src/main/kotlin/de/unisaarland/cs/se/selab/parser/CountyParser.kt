9e21aaa1dcf45e085f5c884441a770ddb249d552
Iryna Kmet <irkm00001@stud.uni-saarland.de>
2023-09-26 23:52:01 +0200

    County parser: parsing 1st part

--- src/main/kotlin/de/unisaarland/cs/se/selab/parser/CountyParser.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/parser/CountyParser.kt
@@ -1,9 +1,15 @@
 package de.unisaarland.cs.se.selab.parser
 
+import de.unisaarland.cs.se.selab.global.Log
 import de.unisaarland.cs.se.selab.graph.Graph
 import de.unisaarland.cs.se.selab.graph.Road
 import de.unisaarland.cs.se.selab.graph.Vertex
+import java.io.BufferedReader
 import java.io.File
+import java.lang.IllegalArgumentException
+import java.util.IllegalFormatException
+import java.util.InvalidPropertiesFormatException
+import java.util.regex.Pattern
 import kotlin.system.exitProcess
 
 /**
@@ -19,20 +25,49 @@ class CountyParser(private val dotFilePath: String) {
         try {
             // Convert path to a file
             this.dotFile = File(dotFilePath)
+            this.data = this.dotFile.readText()
         } catch (_: NullPointerException) {
             // File doesn't exist
             exitProcess(1)
         }
     }
-   fun parse():Graph {
-       // Creating it if the syntax is valid
-       val blueprint = createBlueprint()
-      return Graph()
-   }
+
+    fun parse(): Graph {
+        // Creating it if the syntax is valid
+        val blueprint = createBlueprint()
+
+        return Graph()
+    }
+
+    /**
+     * Calculates the number of occurrences of [char] in the given [str]
+     */
+    private fun countChar(str: String, char: Char): Int {
+        return str.count { it == char }
+    }
+
+    private fun parseVerticesAndEdges(str: String): Map<String, String> {
+
+    }
 
     private fun createBlueprint(): Map<String, String> {
         val blueprint = mutableMapOf<String, String>()
-this.
+        try {
+            if (countChar(this.data, '{') == 1 && countChar(this.data, '}') == 1) {
+                val regexAll =
+                    "\\A(?:digraph)(?:[\\s]+)(?:[a-zA-Z]+)(((?:[\\s]+)[{])|[{])(?:[(?:[\\s]+)|(?:[\\S]+)]+)[}]((\\Z)|((?:[\\s]+)\\Z))".toRegex()
+                regexAll.matchEntire(this.data) ?: throw IllegalArgumentException()
+                val start = this.data.indexOf("{")
+                val end = this.data.indexOf("}")
+                val subs = this.data.substring(start + 1, end)
+                blueprint.putAll(parseVerticesAndEdges(subs))
+            } else {
+                throw IllegalArgumentException()
+            }
+        } catch (_: Exception) {
+            Log.displayInitializationInfoInvalid(this.dotFile.name)
+            exitProcess(1)
+        }
         return blueprint
     }
 
