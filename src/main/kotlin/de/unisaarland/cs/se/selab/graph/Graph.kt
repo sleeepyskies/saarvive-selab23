700b3abde8afd2a21fef77e9203027d37c212ec9
Umama <umri00001@stud.uni-saarland.de>
2023-09-26 15:57:33 +0200

    changed graph construction to incldue a list of roads

--- src/main/kotlin/de/unisaarland/cs/se/selab/graph/Graph.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/graph/Graph.kt
@@ -14,8 +14,9 @@ import java.util.PriorityQueue
 /**
  * Holds the data for the simulation graph consisting of vertices and roads.
  * @param graph A list of vertices containing connecting roads
+ * @param roads A list of all the roads in the graph
  */
-class Graph(private val graph: List<Vertex>) {
+class Graph(private val graph: List<Vertex>, private val roads: List<Road>) {
     /**
      * Returns the shortest time in ticks needed to travel from start the vertex
      * to the destination vertex
