9a8cd96fa776148ac9b1419dbd602237acfa65fb
Umama <umri00001@stud.uni-saarland.de>
2023-09-26 11:48:21 +0200

    tying to solve merge reuqest

--- src/main/kotlin/de/unisaarland/cs/se/selab/dataClasses/bases/Base.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/dataClasses/bases/Base.kt
@@ -1,7 +1,11 @@
-﻿package de.unisaarland.cs.se.selab.dataClasses.bases
+package de.unisaarland.cs.se.selab.dataClasses
+
 import de.unisaarland.cs.se.selab.dataClasses.vehicles.Vehicle
 
-abstract class Base( // b
+/**
+ * abstract class that defines a base
+ */
+abstract class Base(
     private val baseID: Int,
     private var staff: Int,
     private val vertexID: String,
