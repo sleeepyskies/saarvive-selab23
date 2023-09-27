6622f8ef78d19bfbb1692aa34889302f3f274d0d
ykamila <ykamila23@gmail.com>
2023-09-27 22:31:16 +0200

    resolved detekt errors

--- src/main/kotlin/de/unisaarland/cs/se/selab/dataClasses/events/TrafficJam.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/dataClasses/events/TrafficJam.kt
@@ -5,16 +5,14 @@ import de.unisaarland.cs.se.selab.graph.Road
 /**
  * Creates an object of the TrafficJam Event, inherits from Event abstract class.
  * Takes the [factor] by which the length of a road is changed during the event;
- * [sourceID] - the source vertex of a road the event influences
- * [targetID] - the target vertex of a road the event influences
  */
 class TrafficJam(
     eventID: Int,
     duration: Int,
     startTick: Int,
-    public val factor: Int,
-    public val startVertex: Int,
-    public val endVertex: Int
+    val factor: Int,
+    val startVertex: Int,
+    val endVertex: Int
 ) : Event(eventID, duration, startTick) {
-    public lateinit var affectedRoad: Road
+    lateinit var affectedRoad: Road
 }
