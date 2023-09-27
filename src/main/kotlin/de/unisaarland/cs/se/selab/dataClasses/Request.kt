61226cc4132ab78f7758bac65a7117071139f942
htutwin <miwi00005@stud.uni-saarland.de>
2023-09-27 12:06:53 +0200

    updated request class imports + made all fields public instead of private

--- src/main/kotlin/de/unisaarland/cs/se/selab/dataClasses/Request.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/dataClasses/Request.kt
@@ -1,12 +1,14 @@
 package de.unisaarland.cs.se.selab.dataClasses
 
+import de.unisaarland.cs.se.selab.dataClasses.vehicles.CapacityType
+import de.unisaarland.cs.se.selab.dataClasses.vehicles.VehicleType
+
 /** A request that is created whe there are no enough assets to handle an emergency.
  */
 data class Request(
-    private val baseIDsToVisit: List<Int>,
-    private val emergencyID: Int,
-    private val requestID: Int,
-    private val requiredVehicles: Map<VehicleType, Int>,
-    private val requiredCapacity: Map<CapacityType, Int>
+    val baseIDsToVisit: List<Int>,
+    val emergencyID: Int,
+    val requestID: Int,
+    val requiredVehicles: Map<VehicleType, Int>,
+    val requiredCapacity: Map<CapacityType, Int>
 )
-
