b693b479baee44965d31de2442659e91909ec0c2
htutwin <miwi00005@stud.uni-saarland.de>
2023-09-27 15:04:49 +0200

    added documentation for emergencystatus

--- src/main/kotlin/de/unisaarland/cs/se/selab/dataClasses/emergencies/EmergencyStatus.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/dataClasses/emergencies/EmergencyStatus.kt
@@ -1,5 +1,8 @@
 package de.unisaarland.cs.se.selab.dataClasses.emergencies
 
+/**
+ * enum class that represents different emergency statuses
+ */
 enum class EmergencyStatus {
     UNASSIGNED,
     ASSIGNED,
