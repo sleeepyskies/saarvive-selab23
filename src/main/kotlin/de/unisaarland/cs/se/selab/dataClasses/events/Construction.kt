c7b6f96e5a9147788426c55a4bd49bf4543fa851
Umama <umri00001@stud.uni-saarland.de>
2023-09-27 11:40:17 +0200

    removed redundant modifiers

--- src/main/kotlin/de/unisaarland/cs/se/selab/dataClasses/events/Construction.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/dataClasses/events/Construction.kt
@@ -12,11 +12,11 @@ class Construction(
     eventID: Int,
     duration: Int,
     startTick: Int,
-    public val factor: Int,
-    public val sourceID: Int, // id of the vertex
-    public val targetID: Int,
-    public val streetClosed: Boolean
+    val factor: Int,
+    val sourceID: Int, // id of the vertex
+    val targetID: Int,
+    val streetClosed: Boolean
 ) : Event(eventID, duration, startTick) {
-    public lateinit var affectedRoad: Road
+    lateinit var affectedRoad: Road
     // oneWayStreet
 }
