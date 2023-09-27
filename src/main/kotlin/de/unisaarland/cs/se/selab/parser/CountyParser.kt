ae434aca101700c5bb11a64f0471f83d43ec93c4
Iryna Kmet <irkm00001@stud.uni-saarland.de>
2023-09-27 22:52:45 +0200

    Parse initialised

--- src/main/kotlin/de/unisaarland/cs/se/selab/parser/CountyParser.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/parser/CountyParser.kt
@@ -14,8 +14,11 @@ import kotlin.system.exitProcess
 class CountyParser(private val dotFilePath: String) {
     private val dotFile: File
     private val data: String
-    private val roads: List<Road> = mutableListOf() // List of roads on the map (for compatability)
+    private val roads = mutableListOf<Road>() // List of roads on the map (for compatability)
 
+    /**
+     * Save the file and data in string
+     */
     init {
         try {
             // Convert path to a file
@@ -28,42 +31,68 @@ class CountyParser(private val dotFilePath: String) {
         }
     }
 
+    /**
+     * Receives results in parsing, validating, creates objects and returns Graph
+     */
     fun parse(): Graph {
         // Creating it if the syntax is valid
         val blueprint = createBlueprint()
         if (!validateBlueprint(blueprint)) exitProcess(1)
-        return Graph()
+        Log.displayInitializationInfoValid(this.dotFile.name)
+        val roads = createRoadList(blueprint)
+        this.roads.addAll(roads)
+        val vertices = createVertexList(blueprint)
+        connectVertices(vertices, roads, blueprint)
+        return createGraph(vertices, roads)
     }
 
 
     /**
-     * Creates blueprint of id or id->if for Vertices and Roads
+     * Creates blueprint of digraph - name, id, id->id for Vertices and Roads
      */
     private fun createBlueprint(): Map<String, String> {
         val blueprint = mutableMapOf<String, String>()
 
     }
 
+    /**
+     * Return result in validating a [blueprint] (successful or not)
+     */
     private fun validateBlueprint(blueprint: Map<String, String>): Boolean {
 
     }
 
-    private fun createGraph(vertices: List<Vertex>): Graph {
+    /**
+     * Creates object of Graph with [vertices] and [roads]
+     */
+    private fun createGraph(vertices: List<Vertex>, roads: List<Road>): Graph {
 
     }
 
-    protected fun createRoad(road: String): Road {
+    /**
+     * Creates single object of Road out of [road] data
+     */
+    private fun createRoad(road: String): Road {
 
     }
 
+    /**
+     * Returns list of Roads out of [blueprint] data
+     */
     private fun createRoadList(blueprint: Map<String, String>): List<Road> {
 
     }
 
+    /**
+     * Returns list of Vertices out of [blueprint] data
+     */
     private fun createVertexList(blueprint: Map<String, String>): List<Vertex> {
 
     }
 
+    /**
+     * Creates single object of Vertex out of [vertex] data
+     */
     private fun createVertex(vertex: String): Vertex {
 
     }
