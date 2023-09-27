7186449be191b9fa3a6394a5342aa68b31c39c4d
skysw <skwa00001@stud.uni-saarland.de>
2023-09-27 15:16:23 +0200

    Added more VehicleUpdatePhase functionality

--- src/main/kotlin/de/unisaarland/cs/se/selab/phases/VehicleUpdatePhase.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/phases/VehicleUpdatePhase.kt
@@ -22,7 +22,8 @@ class VehicleUpdatePhase(private val dataHolder: DataHolder) {
     }
 
     /**
-     * Moves a vehicle along its current route
+     * Moves a vehicle along its current route, and checks whether the vehicle
+     * has reached the end of a road or route.
      */
     private fun updateVehiclePosition(vehicle: Vehicle) {
         // move vehicle 1 tick along road
@@ -30,13 +31,40 @@ class VehicleUpdatePhase(private val dataHolder: DataHolder) {
 
         // check if vehicle has reached the end of a road
         if (vehicle.roadProgress == 0) {
-            // check if vehicle has reached the end of its route
-            if (vehicle.currentRoute.size == 1) {
-                vehicle.status = VehicleStatus.ARRIVED
+            vehicle.currentRoute.removeAt(0)
+            vehicle.lastVisitedVertex = vehicle.currentRoute[0]
+
+            // check if vehicle has reached its destination
+            if (vehicle.currentRoute.size == 0) {
+                // check if vehicle is moving to emergency or to base
+                if (vehicle.vehicleStatus == VehicleStatus.MOVING_TO_EMERGENCY) {
+                    vehicle.vehicleStatus = VehicleStatus.ARRIVED
+                    dataHolder.emergencyToVehicles[vehicle.assignedEmergencyID]!!.add(vehicle)
+                    val requiredVehicles = dataHolder.vehicleToEmergency[vehicle.id]!!.requiredVehicles
+                    requiredVehicles[vehicle.vehicleType] = requiredVehicles[vehicle.vehicleType]!! - 1
+                    if (requiredVehicles[vehicle.vehicleType] == 0) {
+                        requiredVehicles.remove(vehicle.vehicleType)
+                    }
+                } else if (vehicle.vehicleStatus == VehicleStatus.MOVING_TO_BASE) {
+                    // check if vehicle needs to recharge
+                    dataHolder.activeVehicles.remove(vehicle)
+
+                }
             } else {
-                vehicle.lastVisitedVertex = vehicle.currentRoute[0]
-                vehicle.currentRoute.removeAt(0)
+                // assign new roadProgress
+                vehicle.roadProgress = roundTo10(vehicle.lastVisitedVertex.connectingRoads[vehicle.currentRoute[1]]!!.weight)
             }
+        }
+    }
+
+    /**
+     * Returns the number rounded up to the nearest 10
+     */
+    private fun roundTo10 (number: Int): Int {
+        return if (number % 10 == 0) {
+            number // number is already a multiple of ten
+        } else {
+            number + (10 - number % 10) // round up
         }
     }
 
@@ -51,13 +79,6 @@ class VehicleUpdatePhase(private val dataHolder: DataHolder) {
      * Updates the given emergency's status
      */
     private fun updateEmergencyStatus(emergency: Emergency, status: EmergencyStatus) {
-
-    }
-
-    /**
-     * Changes the vehicles
-     */
-    private fun updateVehicleAvailability(vehicle: Vehicle) {
 
     }
 }
