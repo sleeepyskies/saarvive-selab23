5cf1d107c4ed37d97cb4702dcf8874ab32eaf43f
skysw <skwa00001@stud.uni-saarland.de>
2023-09-27 11:46:09 +0200

    Added VehicleUpdatePhase skeleton

--- src/main/kotlin/de/unisaarland/cs/se/selab/phases/VehicleUpdatePhase.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/phases/VehicleUpdatePhase.kt
@@ -1,4 +1,39 @@
 package de.unisaarland.cs.se.selab.phases
 
-class VehicleUpdatePhase {
+import de.unisaarland.cs.se.selab.dataClasses.emergencies.Emergency
+import de.unisaarland.cs.se.selab.dataClasses.emergencies.EmergencyStatus
+import de.unisaarland.cs.se.selab.dataClasses.vehicles.Vehicle
+import de.unisaarland.cs.se.selab.dataClasses.vehicles.VehicleStatus
+import de.unisaarland.cs.se.selab.simulation.DataHolder
+
+/**
+ * This phase is responsible for moving all active vehicles,
+ * checks if vehicles have reached their emergency,
+ * and updates both vehicle and emergency statuses
+ */
+class VehicleUpdatePhase(dataHolder: DataHolder) {
+
+    public fun execute() {
+
+    }
+
+    private fun updateVehiclePosition(vehicle: Vehicle) {
+
+    }
+
+    private fun checkVehicleReachedEmergency(vehicle: Vehicle): Boolean {
+
+    }
+
+    private fun updateEmergencyStatus(emergency: Emergency, status: EmergencyStatus) {
+
+    }
+
+    private fun updateVehicleAvailability(vehicle: Vehicle) {
+
+    }
+
+    private fun updateVehicleStatus(vehicle: Vehicle, status: VehicleStatus) {
+
+    }
 }
