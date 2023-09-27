868c5cb7dd1825997a87a9869fad5149622d1dfc
htutwin <miwi00005@stud.uni-saarland.de>
2023-09-27 12:32:07 +0200

    changed vehicle class

--- src/main/kotlin/de/unisaarland/cs/se/selab/dataClasses/vehicles/Vehicle.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/dataClasses/vehicles/Vehicle.kt
@@ -13,23 +13,20 @@ open class Vehicle(
     open val height: Int,
     open val assignedBaseID: Int
 ) {
-    var status: VehicleStatus = VehicleStatus.IN_BASE
-    public var isAvailable = true
-    public var assignedEmergencyID: Int? = null
-    public var currentRoad: Road? = null
-    public var lastVisitedVertex: Vertex? = null
-    public var ticksTillDestination: Int = 0
-    public var roadProgrss: Int = 0
-    public var currentRoute: MutableList<Vertex> = mutableListOf<Vertex>()
-    public var ticksStillUnavailable: Int = 0
+    open var vehicleStatus: VehicleStatus = VehicleStatus.IN_BASE
+    open var isAvailable = true
+    open var assignedEmergencyID: Int? = null
+    open var currentRoad: Road? = null
+    open var lastVisitedVertex: Vertex? = null
+    open var ticksTillDestination: Int = 0
+    open var roadProgress: Int = 0
+    open var currentRoute: MutableList<Vertex> = mutableListOf<Vertex>()
+    open var ticksStillUnavailable: Int = 0
 
     /**
      * Returns the status of the vehicle
      */
     fun getVehicleStatus(): VehicleStatus {
-        return status
+        return vehicleStatus
     }
 }
-
-
-
