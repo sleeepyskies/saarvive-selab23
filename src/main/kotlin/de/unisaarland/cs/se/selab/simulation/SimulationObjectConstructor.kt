60e03824d1f9848277f1ec4406d0d1b3ea9c223e
skysw <skwa00001@stud.uni-saarland.de>
2023-09-28 11:58:34 +0200

    Added SimulationObjectConstructor skeleton

---
+++ src/main/kotlin/de/unisaarland/cs/se/selab/simulation/SimulationObjectConstructor.kt
@@ -0,0 +1,41 @@
+package de.unisaarland.cs.se.selab.simulation
+
+import de.unisaarland.cs.se.selab.dataClasses.bases.Base
+import de.unisaarland.cs.se.selab.dataClasses.emergencies.Emergency
+import de.unisaarland.cs.se.selab.dataClasses.events.Event
+import de.unisaarland.cs.se.selab.graph.Graph
+
+/**
+ * Is responsible for calling the parser classes
+ */
+class SimulationObjectConstructor(
+    private val countyFile: String,
+    private val assetFile: String,
+    private val simulationFile: String,
+    private val maxTick: Int?
+) {
+    public fun createSimulation(): Simulation {
+
+    }
+
+    private fun createDataHolder(
+        graph: Graph, bases: List<Base>,
+        events: MutableList<Event>,
+        emergencies: MutableList<Emergency>
+    ): DataHolder {
+
+    }
+
+    private fun validateAssetsBasedOnGraph(graph: Graph, bases: List<Base>): Boolean {
+
+    }
+
+    private fun validateEventsBasedOnGraph(graph: Graph, events: List<Event>): Boolean {
+
+    }
+
+    private fun validateEmergenciesBasedOnGraph(graph: Graph, emergencies: Emergency): Boolean {
+
+    }
+
+}
