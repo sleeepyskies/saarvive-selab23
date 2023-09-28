ad74ad7810d40088c53b1a2eb2786d7e1091bcdb
skysw <skwa00001@stud.uni-saarland.de>
2023-09-28 13:56:31 +0200

    Finised createSimulation() method in SOC

--- src/main/kotlin/de/unisaarland/cs/se/selab/simulation/SimulationObjectConstructor.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/simulation/SimulationObjectConstructor.kt
@@ -5,6 +5,10 @@ import de.unisaarland.cs.se.selab.dataClasses.emergencies.Emergency
 import de.unisaarland.cs.se.selab.dataClasses.events.Event
 import de.unisaarland.cs.se.selab.graph.Graph
 import de.unisaarland.cs.se.selab.graph.Vertex
+import de.unisaarland.cs.se.selab.parser.AssetParser
+import de.unisaarland.cs.se.selab.parser.CountyParser
+import de.unisaarland.cs.se.selab.parser.SimulationParser
+import kotlin.system.exitProcess
 
 /**
  * Is responsible for calling the parsers, cross
@@ -17,7 +21,33 @@ class SimulationObjectConstructor(
     private val maxTick: Int?
 ) {
     public fun createSimulation(): Simulation {
-        //code
+        // parse, validate and create map
+        val countyParser = CountyParser(countyFile)
+        val graph = countyParser.parse()
+
+        // parse, validate and create assets
+        val assetParser = AssetParser("src/main/resources/schema/assets.schema",assetFile)
+        val assets = assetParser.parse()
+        val bases = assets.first
+
+        // parse, validate and create events and emergencies
+        val simulationParser = SimulationParser("src/main/resources/schema/simulation.schema",simulationFile)
+        val emergencies = simulationParser.parseEmergencyCalls()
+        val events = simulationParser.parseEvents()
+
+        // cross validation and construction
+        if (
+            validateAssetsBasedOnGraph(graph, bases) &&
+            validateEmergenciesBasedOnGraph(graph, emergencies) &&
+            validateEventsBasedOnGraph(graph, events)
+            ) {
+            // If validation succeeds return simulation
+            val dataHolder = DataHolder(graph, bases, events.toMutableList(), emergencies)
+            return Simulation(dataHolder, maxTick)
+        } else {
+            // If validation fails exit
+            exitProcess(1)
+        }
     }
 
     /**
@@ -59,14 +89,16 @@ class SimulationObjectConstructor(
     /**
      * Cross validates the events based on the graph
      */
-    private fun validateEventsBasedOnGraph(graph: Graph, events: List<Event>) {
+    private fun validateEventsBasedOnGraph(graph: Graph, events: List<Event>): Boolean {
+        for (event in events) {
 
+        }
     }
 
     /**
      * Cross validates the emergencies based on the graph
      */
-    private fun validateEmergenciesBasedOnGraph(graph: Graph, emergencies: Emergency) {
+    private fun validateEmergenciesBasedOnGraph(graph: Graph, emergencies: List<Emergency>): Boolean {
 
     }
 
