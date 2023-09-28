89ab914acf4b8f1f52b295b4eff3b491b512c893
ykamila <ykamila23@gmail.com>
2023-09-28 16:19:46 +0200

    fixed detect errors

--- src/main/kotlin/de/unisaarland/cs/se/selab/phases/AllocationPhase.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/phases/AllocationPhase.kt
@@ -17,7 +17,7 @@ import de.unisaarland.cs.se.selab.simulation.DataHolder
  *
  */
 class AllocationPhase(private val dataHolder: DataHolder) : Phase {
-    private val currentTick = 0
+    private var currentTick = 0
     private val nextRequestId = 0
 
     /** Executes the allocation phase
