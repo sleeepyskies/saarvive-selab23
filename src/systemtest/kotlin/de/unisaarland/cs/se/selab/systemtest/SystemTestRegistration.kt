4058bc443da1f9fce0e46f7dd00e107cfc905592
skysw <skwa00001@stud.uni-saarland.de>
2023-09-29 13:52:22 +0200

    Added more system tests

--- src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/SystemTestRegistration.kt
+++ src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/SystemTestRegistration.kt
@@ -1,10 +1,14 @@
 package de.unisaarland.cs.se.selab.systemtest
 
+import de.unisaarland.cs.se.selab.systemtest.basictests.CorrectSystemTestSmall
+import de.unisaarland.cs.se.selab.systemtest.basictests.EmergencyFailTest
 import de.unisaarland.cs.se.selab.systemtest.basictests.ExampleTest
 import de.unisaarland.cs.se.selab.systemtest.runner.SystemTestManager
 
 object SystemTestRegistration {
     fun registerSystemTests(manager: SystemTestManager) {
         manager.registerTest(ExampleTest())
+        manager.registerTest(CorrectSystemTestSmall())
+        manager.registerTest(EmergencyFailTest())
     }
 }
--- src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/basictests/EmergencyOngoingTest.kt
+++ src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/basictests/EmergencyOngoingTest.kt
@@ -2,13 +2,13 @@ package de.unisaarland.cs.se.selab.systemtest.basictests
 
 import de.unisaarland.cs.se.selab.systemtest.api.SystemTest
 
-class EmergencyFailTest : SystemTest() {
-    override val name = "EmergencyFailTest"
+class EmergencyOngoingTest : SystemTest() {
+    override val name = "EmergencyOngoingTest"
 
     override val map = "mapFiles/emergencyFailTestMap.dot"
     override val assets = "assetsJsons/emergencyFailTestAssets.json"
     override val scenario = "scenarioJsons/emergencyFailTestScenario.json"
-    override val maxTicks = 6
+    override val maxTicks = 2
 
     override suspend fun run() {
         // Everything is parsed and validated
@@ -24,25 +24,14 @@ class EmergencyFailTest : SystemTest() {
         assertNextLine("Simulation Tick: 1")
         assertNextLine("Emergency Assignment: 0 assigned to 0")
         assertNextLine("Asset Allocation: 0 allocated to 0; 3 ticks to arrive.")
-        // Tick 2
-        assertNextLine("Simulation Tick: 2")
-        assertNextLine("Event Ended: 0 ended.")
-        // Tick 3
-        assertNextLine("Simulation Tick: 3")
-        assertNextLine("Asset Arrival: 0 arrived at 3.")
-        // Tick 4
-        assertNextLine("Simulation Tick: 4")
-        // Tick 5
-        assertNextLine("Simulation Tick: 5")
-        assertNextLine("Emergency Failed: 0 failed.")
 
         // Simulation End
         assertNextLine("Simulation End")
         // Simulation Statistics
         assertNextLine("Simulation Statistics: 0 assets rerouted.")
         assertNextLine("Simulation Statistics: 1 received emergencies.")
-        assertNextLine("Simulation Statistics: 0 ongoing emergencies.")
-        assertNextLine("Simulation Statistics: 1 failed emergencies.")
+        assertNextLine("Simulation Statistics: 1 ongoing emergencies.")
+        assertNextLine("Simulation Statistics: 0 failed emergencies.")
         assertNextLine("Simulation Statistics: 0 resolved emergencies.")
         // File End has been reached
         assertEnd()
