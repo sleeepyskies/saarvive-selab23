841bf6acd56dbf12496106b6bf6bf2e7af65197c
skysw <skwa00001@stud.uni-saarland.de>
2023-09-29 14:07:15 +0200

    Added more faulty system tests

--- src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/SystemTestRegistration.kt
+++ src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/SystemTestRegistration.kt
@@ -1,8 +1,10 @@
 package de.unisaarland.cs.se.selab.systemtest
 
 import de.unisaarland.cs.se.selab.systemtest.basictests.ExampleTest
+import de.unisaarland.cs.se.selab.systemtest.mysystemtest.faultytests.NonExistentVertexTest
 import de.unisaarland.cs.se.selab.systemtest.mysystemtest.faultytests.TwoRoadsSameLocationTest
 import de.unisaarland.cs.se.selab.systemtest.mysystemtest.faultytests.TwoRoadsSameNameTest
+import de.unisaarland.cs.se.selab.systemtest.mysystemtest.faultytests.ZeroHeightTest
 import de.unisaarland.cs.se.selab.systemtest.mysystemtest.validtests.CorrectSystemTestSmall
 import de.unisaarland.cs.se.selab.systemtest.mysystemtest.validtests.EmergencyFailTest
 import de.unisaarland.cs.se.selab.systemtest.runner.SystemTestManager
@@ -14,5 +16,7 @@ object SystemTestRegistration {
         manager.registerTest(EmergencyFailTest())
         manager.registerTest(TwoRoadsSameLocationTest())
         manager.registerTest(TwoRoadsSameNameTest())
+        manager.registerTest(NonExistentVertexTest())
+        manager.registerTest(ZeroHeightTest())
     }
 }
---
+++ src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/mysystemtest/faultytests/NegativeVertexTest.kt
@@ -0,0 +1,16 @@
+package de.unisaarland.cs.se.selab.systemtest.mysystemtest.faultytests
+
+import de.unisaarland.cs.se.selab.systemtest.api.SystemTest
+
+class NegativeVertexTest : SystemTest() {
+    override val name = "NegativeVertexTest"
+
+    override val map = "mapFiles/mapFiles/negativeVertexID.dot"
+    override val assets = "assetsJsons/validBaseFileSmall.json"
+    override val scenario = "scenarioJsons/validScenarioFileSmall.json"
+    override suspend fun run() {
+        assertNextLine("Initialization Info: negativeVertexID.dot invalid")
+    }
+
+    override val maxTicks = 5
+}
