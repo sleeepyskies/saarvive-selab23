fdceb254fc58aa6c85dc868ae8c29841e06a7156
skysw <skwa00001@stud.uni-saarland.de>
2023-09-29 14:00:36 +0200

    Added more system tests

--- src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/SystemTestRegistration.kt
+++ src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/SystemTestRegistration.kt
@@ -2,6 +2,7 @@ package de.unisaarland.cs.se.selab.systemtest
 
 import de.unisaarland.cs.se.selab.systemtest.basictests.ExampleTest
 import de.unisaarland.cs.se.selab.systemtest.mysystemtest.faultytests.TwoRoadsSameLocationTest
+import de.unisaarland.cs.se.selab.systemtest.mysystemtest.faultytests.TwoRoadsSameNameTest
 import de.unisaarland.cs.se.selab.systemtest.mysystemtest.validtests.CorrectSystemTestSmall
 import de.unisaarland.cs.se.selab.systemtest.mysystemtest.validtests.EmergencyFailTest
 import de.unisaarland.cs.se.selab.systemtest.runner.SystemTestManager
@@ -12,5 +13,6 @@ object SystemTestRegistration {
         manager.registerTest(CorrectSystemTestSmall())
         manager.registerTest(EmergencyFailTest())
         manager.registerTest(TwoRoadsSameLocationTest())
+        manager.registerTest(TwoRoadsSameNameTest())
     }
 }
