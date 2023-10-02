44dd5a93bb3fb4eb888ecd6f55db6770029bb7c2
htutwin <miwi00005@stud.uni-saarland.de>
2023-10-02 15:29:24 +0200

    fixed assetparsertest to fit new assetparser (new rules)

--- src/test/kotlin/parsertests/AssetParserTest.kt
+++ src/test/kotlin/parsertests/AssetParserTest.kt
@@ -4,8 +4,6 @@ import de.unisaarland.cs.se.selab.dataClasses.bases.FireStation
 import de.unisaarland.cs.se.selab.dataClasses.vehicles.VehicleType
 import de.unisaarland.cs.se.selab.parser.AssetParser
 import org.json.JSONException
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import java.io.FileNotFoundException
@@ -16,7 +14,7 @@ class AssetParserTest {
     fun testValidAssetBASE() {
         val parser = AssetParser(
             assetSchemaFile = "base.schema",
-            assetJsonFile = "src/test/resources/parsertests/assetParser/validAssets.json"
+            assetJsonFile = "src/systemtest/resources/assetsJsons/example_assets.json"
         )
 
         val (_, bases) = parser.parse()
@@ -24,25 +22,28 @@ class AssetParserTest {
         // Validate the first base
         val base1 = bases[0]
         assert(base1 is FireStation)
-        assert(base1.baseID == 1)
-        assert(base1.vertexID == 100)
-        assert(base1.staff == 20)
+        assert(base1.baseID == 0)
+        assert(base1.vertexID == 2)
+        assert(base1.staff == 62)
     }
 
     @Test
     fun testValidAssetVEHICLE() {
         val parser = AssetParser(
             assetSchemaFile = "vehicle.schema",
-            assetJsonFile = "src/test/resources/parsertests/assetParser/validAssets.json"
+            assetJsonFile = "src/systemtest/resources/assetsJsons/example_assets.json"
         )
 
         val (vehicles, _) = parser.parse()
 
         // Validate the first vehicle
         val vehicle1 = vehicles[0]
-        assert(vehicle1.vehicleType == VehicleType.FIRE_TRUCK_WATER)
-        assert(vehicle1.id == 101)
+        assert(vehicle1.vehicleType == VehicleType.POLICE_CAR)
+        assert(vehicle1.id == 0)
         assert(vehicle1.assignedBaseID == 1)
+        assert(vehicle1.height == 2)
+        assert(vehicle1.staffCapacity == 5)
+        // assert(vehicle1.maxCriminalCapacity == 2) to be fixed
     }
 
     @Test
@@ -77,60 +78,31 @@ class AssetParserTest {
 
     @Test
     fun testEmptyAssetListBASE() {
-        val parser = AssetParser(
-            assetSchemaFile = "base.schema",
-            assetJsonFile = "src/test/resources/parsertests/assetParser/empty_assets.json"
-        )
-
-        val (_, bases) = parser.parse()
-        assertEquals(0, bases.size)
+        assertThrows<IllegalArgumentException> {
+            AssetParser(
+                assetSchemaFile = "base.schema",
+                assetJsonFile = "src/test/resources/parsertests/assetParser/empty_assets.json"
+            ).parse()
+        }
     }
 
     @Test
     fun testEmptyAssetListVEHICLE() {
-        val parser = AssetParser(
-            assetSchemaFile = "vehicle.schema",
-            assetJsonFile = "src/test/resources/parsertests/assetParser/empty_assets.json"
-        )
-
-        val (vehicles, _) = parser.parse()
-        assertEquals(0, vehicles.size)
-    }
-
-    @Test
-    fun testSingleAssetBASE() {
-        val parser = AssetParser(
-            assetSchemaFile = "base.schema",
-            assetJsonFile = "src/test/resources/parsertests/assetParser/single_asset.json"
-        )
-
-        // validate single base
-        val (_, bases) = parser.parse()
-        assertEquals(1, bases.size)
-
-        val singleBase = bases[0]
-        assertTrue(singleBase is FireStation)
-        assertEquals(0, singleBase.baseID)
-        assertEquals(2, singleBase.vertexID)
-        assertEquals(62, singleBase.staff)
+        assertThrows<IllegalArgumentException> {
+            AssetParser(
+                assetSchemaFile = "vehicle.schema",
+                assetJsonFile = "src/test/resources/parsertests/assetParser/empty_assets.json"
+            ).parse()
+        }
     }
 
     @Test
     fun testSingleAssetVEHICLE() {
-        val parser = AssetParser(
-            assetSchemaFile = "vehicle.schema",
-            assetJsonFile = "src/test/resources/parsertests/assetParser/single_asset.json"
-        )
-
-        // validate single vehicle
-        val (vehicles, _) = parser.parse()
-        assertEquals(1, vehicles.size)
-
-        val singleVehicle = vehicles.first()
-        assertEquals(VehicleType.FIRE_TRUCK_WATER, singleVehicle.vehicleType)
-        assertEquals(0, singleVehicle.id)
-        assertEquals(0, singleVehicle.assignedBaseID)
-        assertEquals(2, singleVehicle.height)
-        assertEquals(5, singleVehicle.staffCapacity)
+        assertThrows<IllegalArgumentException> {
+            AssetParser(
+                assetSchemaFile = "vehicle.schema",
+                assetJsonFile = "src/test/resources/parsertests/assetParser/single_asset.json"
+            ).parse()
+        }
     }
 }
