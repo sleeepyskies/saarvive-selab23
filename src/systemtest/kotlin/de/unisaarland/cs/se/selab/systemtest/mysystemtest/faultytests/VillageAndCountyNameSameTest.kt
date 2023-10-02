70231efc94bf03815d8c405d98bcd22486de58e9
Umama <umri00001@stud.uni-saarland.de>
2023-10-02 09:40:36 +0200

    added test

---
+++ src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/mysystemtest/faultytests/VillageAndCountyNameSameTest.kt
@@ -0,0 +1,16 @@
+package de.unisaarland.cs.se.selab.systemtest.mysystemtest.faultytests
+
+import de.unisaarland.cs.se.selab.systemtest.api.SystemTest
+
+class VillageAndCountyNameSameTest : SystemTest() {
+    override val name = "VillageAndCountyNameSameTest"
+
+    override val map = "mapFiles/villageAndCountyNameSame.dot"
+    override val assets = "assetsJsons/validBaseFileSmall.json"
+    override val scenario = "scenarioJsons/validScenarioFileSmall.json"
+    override suspend fun run() {
+        assertNextLine("Initialization Info: villageAndCountyNameSame.dot invalid")
+    }
+
+    override val maxTicks = 5
+}
