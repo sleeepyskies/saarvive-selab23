8054130d3bb18cc7573d9ba1c7357829f780a7c7
Sky Waller <skyswalla@gmail.com>
2023-10-02 10:51:38 +0200

    Added Graph unit test

--- src/main/kotlin/de/unisaarland/cs/se/selab/graph/GraphHelper.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/graph/GraphHelper.kt
@@ -53,9 +53,9 @@ class GraphHelper {
     private fun weightToTicks(weight: Int): Int {
         if (weight < Number.TEN) return 1
         return if (weight % Number.TEN == 0) {
-            weight // number is already a multiple of ten
+            weight / Number.TEN // number is already a multiple of ten
         } else {
-            weight + (Number.TEN - weight % Number.TEN) // round up
+            (weight + (Number.TEN - weight % Number.TEN)) / Number.TEN // round up
         }
     }
 
