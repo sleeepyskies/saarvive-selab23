cc72f9459b09c4db0434ee23d67202e2794c3dac
Umama <umri00001@stud.uni-saarland.de>
2023-10-02 09:40:47 +0200

    modified tst file

--- src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/mysystemtest/faultytests/SameVertexDifferentVillagesTest.kt
+++ src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/mysystemtest/faultytests/SameVertexDifferentVillagesTest.kt
@@ -5,11 +5,11 @@ import de.unisaarland.cs.se.selab.systemtest.api.SystemTest
 class SameVertexDifferentVillagesTest : SystemTest() {
     override val name = "SameVertexDifferentVillagesTest"
 
-    override val map = "mapFiles/sameVertexDifferentVillagesTest.dot"
+    override val map = "mapFiles/sameVertexDifferentVillages.dot"
     override val assets = "assetsJsons/validBaseFileSmall.json"
     override val scenario = "scenarioJsons/validScenarioFileSmall.json"
     override suspend fun run() {
-        assertNextLine("Initialization Info: sameVertexDifferentVillagesTest.dot invalid")
+        assertNextLine("Initialization Info: sameVertexDifferentVillages.dot invalid")
     }
 
     override val maxTicks = 5
