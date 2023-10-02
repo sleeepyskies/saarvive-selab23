a2d143743370f73e073dc31ecefb17e11ea40b32
Umama <umri00001@stud.uni-saarland.de>
2023-10-02 10:29:57 +0200

    modified map test

--- src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/mysystemtest/faultytests/map/RoadHas2SameVerticesTest.kt
+++ src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/mysystemtest/faultytests/map/RoadHas2SameVerticesTest.kt
@@ -5,11 +5,11 @@ import de.unisaarland.cs.se.selab.systemtest.api.SystemTest
 class RoadHas2SameVerticesTest : SystemTest() {
     override val name = "RoadHas2SameVerticesTest"
 
-    override val map = "mapFiles/roadHas2SameVertex.dot"
+    override val map = "mapFiles/roadHas2sameVertex.dot"
     override val assets = "assetsJsons/validBaseFileSmall.json"
     override val scenario = "scenarioJsons/validScenarioFileSmall.json"
     override suspend fun run() {
-        assertNextLine("Initialization Info: noSideStreet.dot invalid")
+        assertNextLine("Initialization Info: roadHas2sameVertex.dot invalid")
     }
 
     override val maxTicks = 5
