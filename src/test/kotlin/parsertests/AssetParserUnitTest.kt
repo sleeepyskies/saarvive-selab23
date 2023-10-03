8001ce51542bc967dd64990c319d4918282f5152
ykamila <ykamila23@gmail.com>
2023-10-03 14:54:57 +0200

    resolved detekt errors

--- src/test/kotlin/parsertests/AssetParserUnitTest.kt
+++ src/test/kotlin/parsertests/AssetParserUnitTest.kt
@@ -18,29 +18,29 @@ class AssetParserUnitTest {
     }
 
     @Test
-    fun `test id validation`(){
+    fun `test id validation`() {
         assertThrows<IllegalArgumentException> { assetParser.validateBaseId(Random.nextInt(-1000, -1)) }
     }
 
-     @Test
-     fun `test base validation`() {
-         val baseType = "FIRE_STATION"
-         val baseType2 = "POLICE_STATION"
-         val baseType3 = "HOSPITAL"
-         val baseType4 = "INVALID_BASE_TYPE"
-         val baseType5 = "BASE"
-         val baseType6 = "MINECRAFT"
-         val baseType7 = "GHH"
-         val baseType8 = ""
-         val baseType9 = "     "
-         assertEquals(baseType, assetParser.validateBaseType(baseType))
-         assertEquals(baseType2, assetParser.validateBaseType(baseType2))
-         assertEquals(baseType3, assetParser.validateBaseType(baseType3))
-         assertThrows<IllegalArgumentException> { assetParser.validateBaseType(baseType4) }
-         assertThrows<IllegalArgumentException> { assetParser.validateBaseType(baseType5) }
-         assertThrows<IllegalArgumentException> { assetParser.validateBaseType(baseType6) }
-         assertThrows<IllegalArgumentException> { assetParser.validateBaseType(baseType7) }
-         assertThrows<IllegalArgumentException> { assetParser.validateBaseType(baseType8) }
-         assertThrows<IllegalArgumentException> { assetParser.validateBaseType(baseType9) }
-     }
+    @Test
+    fun `test base validation`() {
+        val baseType = "FIRE_STATION"
+        val baseType2 = "POLICE_STATION"
+        val baseType3 = "HOSPITAL"
+        val baseType4 = "INVALID_BASE_TYPE"
+        val baseType5 = "BASE"
+        val baseType6 = "MINECRAFT"
+        val baseType7 = "GHH"
+        val baseType8 = ""
+        val baseType9 = "     "
+        assertEquals(baseType, assetParser.validateBaseType(baseType))
+        assertEquals(baseType2, assetParser.validateBaseType(baseType2))
+        assertEquals(baseType3, assetParser.validateBaseType(baseType3))
+        assertThrows<IllegalArgumentException> { assetParser.validateBaseType(baseType4) }
+        assertThrows<IllegalArgumentException> { assetParser.validateBaseType(baseType5) }
+        assertThrows<IllegalArgumentException> { assetParser.validateBaseType(baseType6) }
+        assertThrows<IllegalArgumentException> { assetParser.validateBaseType(baseType7) }
+        assertThrows<IllegalArgumentException> { assetParser.validateBaseType(baseType8) }
+        assertThrows<IllegalArgumentException> { assetParser.validateBaseType(baseType9) }
+    }
 }
