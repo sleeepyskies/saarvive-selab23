4d219ceb1e2a35f164647af566f864bfff23ed0b
htutwin <miwi00005@stud.uni-saarland.de>
2023-10-03 13:47:21 +0200

    creation of EventsParserTest and its respective json files

---
+++ src/test/kotlin/parsertests/EventsParserTest.kt
@@ -0,0 +1,248 @@
+package parsertests
+
+import de.unisaarland.cs.se.selab.dataClasses.events.RushHour
+import de.unisaarland.cs.se.selab.parser.EventsParser
+import org.json.JSONException
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import java.io.FileNotFoundException
+
+class EventsParserTest {
+
+    @Test
+    fun testValidEventsJSON() {
+        val parser = EventsParser(
+            schemaFile = "event.schema",
+            jsonFile = "src/systemtest/resources/scenarioJsons/example_scenario.json",
+            vehicles = listOf() // Provide a list of vehicles here
+        )
+
+        val events = parser.parse()
+
+        // Validate the first event
+        val event1 = events[0]
+        assert(event1.eventID == 0)
+        assert(event1 is RushHour)
+        assert(event1.startTick == 3)
+        assert(event1.duration == 2)
+        // if (event1 is RushHour) {assert(event1.roadType == ["SIDE_STREET"])}
+        if (event1 is RushHour) { assert(event1.factor == 2) }
+    }
+
+    @Test
+    fun testInvalidPathToJSON() {
+        assertThrows<FileNotFoundException> {
+            EventsParser(
+                schemaFile = "event.schema",
+                jsonFile = "invalid/path/to/json",
+                vehicles = listOf()
+            )
+        }
+    }
+
+    @Test
+    fun testInvalidEventsJSON() {
+        assertThrows<JSONException> {
+            EventsParser(
+                schemaFile = "event.schema",
+                jsonFile = "src/test/resources/parsertests/eventsParser/invalid_events.json",
+                vehicles = listOf()
+            ).parse()
+        }
+    }
+
+    @Test
+    fun testInvalidEventType() {
+        assertThrows<IllegalArgumentException> {
+            EventsParser(
+                schemaFile = "event.schema",
+                jsonFile = "src/test/resources/parsertests/eventsParser/invalid_event_type.json",
+                vehicles = listOf()
+            ).parse()
+        }
+    }
+
+    @Test
+    fun testNegativeEventID() {
+        assertThrows<IllegalArgumentException> {
+            EventsParser(
+                schemaFile = "event.schema",
+                jsonFile = "src/test/resources/parsertests/eventsParser/negative_event_id.json",
+                vehicles = listOf()
+            ).parse()
+        }
+    }
+
+    @Test
+    fun testDuplicateEventID() {
+        assertThrows<IllegalArgumentException> {
+            EventsParser(
+                schemaFile = "event.schema",
+                jsonFile = "src/test/resources/parsertests/eventsParser/duplicate_event_id.json",
+                vehicles = listOf()
+            ).parse()
+        }
+    }
+
+    @Test
+    fun testMissingRequiredFields() {
+        assertThrows<JSONException> {
+            EventsParser(
+                schemaFile = "event.schema",
+                jsonFile = "src/test/resources/parsertests/eventsParser/missing_required_fields.json",
+                vehicles = listOf()
+            ).parse()
+        }
+    }
+
+    @Test
+    fun testRushHourWithNegativeFactor() {
+        assertThrows<IllegalArgumentException> {
+            EventsParser(
+                schemaFile = "event.schema",
+                jsonFile = "src/test/resources/parsertests/eventsParser/rush_hour_negative_factor.json",
+                vehicles = listOf()
+            ).parse()
+        }
+    }
+
+    // CONSTRUCTION_SITE Event Tests
+    @Test
+    fun testConstructionWithNegativeSourceAndTarget() {
+        assertThrows<IllegalArgumentException> {
+            EventsParser(
+                schemaFile = "event.schema",
+                jsonFile = "src/test/resources/parsertests/eventsParser/construction_negative_source_target.json",
+                vehicles = listOf()
+            ).parse()
+        }
+    }
+
+    @Test
+    fun testConstructionWithMissingSourceOrTarget() {
+        assertThrows<JSONException> {
+            EventsParser(
+                schemaFile = "event.schema",
+                jsonFile = "src/test/resources/parsertests/eventsParser/construction_missing_source_or_target.json",
+                vehicles = listOf()
+            ).parse()
+        }
+    }
+
+    // VEHICLE_UNAVAILABLE Event Tests
+    @Test
+    fun testVehicleUnavailableWithNegativeVehicleID() {
+        assertThrows<IllegalArgumentException> {
+            EventsParser(
+                schemaFile = "event.schema",
+                jsonFile = "src/test/resources/parsertests/eventsParser/vehicle_unavailable_negative_id.json",
+                vehicles = listOf()
+            ).parse()
+        }
+    }
+
+    @Test
+    fun testVehicleUnavailableWithNonExistentVehicleID() {
+        assertThrows<IllegalArgumentException> {
+            EventsParser(
+                schemaFile = "event.schema",
+                jsonFile = "src/test/resources/parsertests/eventsParser/vehicle_unavailable_non_existent_id.json",
+                vehicles = listOf()
+            ).parse()
+        }
+    }
+
+    // Duration Tests
+    @Test
+    fun testEventWithZeroOrNegativeDuration() {
+        assertThrows<IllegalArgumentException> {
+            EventsParser(
+                schemaFile = "event.schema",
+                jsonFile = "src/test/resources/parsertests/eventsParser/event_negative_duration.json",
+                vehicles = listOf()
+            ).parse()
+        }
+    }
+
+    // Tick Tests
+    @Test
+    fun testEventWithNegativeTick() {
+        assertThrows<IllegalArgumentException> {
+            EventsParser(
+                schemaFile = "event.schema",
+                jsonFile = "src/test/resources/parsertests/eventsParser/event_negative_tick.json",
+                vehicles = listOf()
+            ).parse()
+        }
+    }
+
+    // Factor Tests
+    @Test
+    fun testEventWithZeroOrNegativeFactor() {
+        assertThrows<IllegalArgumentException> {
+            EventsParser(
+                schemaFile = "event.schema",
+                jsonFile = "src/test/resources/parsertests/eventsParser/event_negative_factor.json",
+                vehicles = listOf()
+            ).parse()
+        }
+    }
+
+    // Road Types Tests
+    @Test
+    fun testEventWithInvalidRoadTypes() {
+        assertThrows<IllegalArgumentException> {
+            EventsParser(
+                schemaFile = "event.schema",
+                jsonFile = "src/test/resources/parsertests/eventsParser/event_invalid_road_types.json",
+                vehicles = listOf()
+            ).parse()
+        }
+    }
+
+    @Test
+    fun testEventWithEmptyRoadTypes() {
+        assertThrows<IllegalArgumentException> {
+            EventsParser(
+                schemaFile = "event.schema",
+                jsonFile = "src/test/resources/parsertests/eventsParser/event_empty_road_types.json",
+                vehicles = listOf()
+            ).parse()
+        }
+    }
+
+    // Source and Target Tests
+    @Test
+    fun testEventWithNegativeSourceOrTarget() {
+        assertThrows<IllegalArgumentException> {
+            EventsParser(
+                schemaFile = "event.schema",
+                jsonFile = "src/test/resources/parsertests/eventsParser/event_negative_source_or_target.json",
+                vehicles = listOf()
+            ).parse()
+        }
+    }
+
+    // Vehicle ID Tests
+//    @Test
+//    fun testEventWithValidVehicleID() {
+//        val parser = EventsParser(
+//            schemaFile = "event.schema",
+//            jsonFile = "src/test/resources/parsertests/eventsParser/event_valid_vehicle_id.json",
+//            vehicles = listOf() // Provide a list of vehicles here
+//        )
+//        val events = parser.parse()
+//        if (events is VehicleUnavailable) { assert(events.vehicleID == 1) }
+//    }
+
+    @Test
+    fun testEventWithNegativeVehicleID() {
+        assertThrows<IllegalArgumentException> {
+            EventsParser(
+                schemaFile = "event.schema",
+                jsonFile = "src/test/resources/parsertests/eventsParser/event_negative_vehicle_id.json",
+                vehicles = listOf()
+            ).parse()
+        }
+    }
+}
