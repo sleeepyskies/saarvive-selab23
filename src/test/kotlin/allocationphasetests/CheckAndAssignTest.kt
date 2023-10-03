b5d40b4d99345c9fd2649d3525edd6b1b7e6bb32
Umama <umri00001@stud.uni-saarland.de>
2023-10-03 20:53:25 +0200

    fixed detekt errors

--- src/test/kotlin/allocationphasetests/CheckAndAssignTest.kt
+++ src/test/kotlin/allocationphasetests/CheckAndAssignTest.kt
@@ -1,6 +1,5 @@
 package allocationphasetests
 
-
 import de.unisaarland.cs.se.selab.dataClasses.bases.Base
 import de.unisaarland.cs.se.selab.dataClasses.bases.PoliceStation
 import de.unisaarland.cs.se.selab.dataClasses.emergencies.Emergency
