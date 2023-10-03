a723e0026e0a1cf39755239ec0af3f4999b3784d
Sky Waller <skyswalla@gmail.com>
2023-10-03 09:25:32 +0200

    added seperate reroute method in MUP to avoid rerouting after each event ends/starts

--- src/main/kotlin/de/unisaarland/cs/se/selab/phases/MapUpdatePhase.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/phases/MapUpdatePhase.kt
@@ -11,11 +11,14 @@ import de.unisaarland.cs.se.selab.simulation.DataHolder
  */
 class MapUpdatePhase(private val dataHolder: DataHolder) : Phase {
     private var currentTick = 0
+    private var shouldReroute = false
     override fun execute() {
         val events = dataHolder.events
         if (events.isNotEmpty()) {
             triggerEvent(events)
             reduceEventDuration(events)
+            if (shouldReroute) rerouteVehicles()
+            shouldReroute = false
         }
         this.currentTick++
     }
@@ -25,14 +28,17 @@ class MapUpdatePhase(private val dataHolder: DataHolder) : Phase {
             if (event.startTick == this.currentTick) {
                 dataHolder.graph.applyGraphEvent(event)
                 Log.displayEventStarted(event.eventID)
+                shouldReroute = true
             }
         }
+        shouldReroute = true
     }
 
     private fun endEvent(event: Event) {
         if (event.duration == 0) {
             Log.displayEventEnded(event.eventID)
         }
+        shouldReroute = true
     }
     private fun reduceEventDuration(events: MutableList<Event>) {
         events.forEach { event ->
@@ -50,19 +56,22 @@ class MapUpdatePhase(private val dataHolder: DataHolder) : Phase {
                 event.startTick == currentTick -> {
                     dataHolder.graph.applyGraphEvent(event)
                     // Log.displayEventStarted(event.eventID)
-                    dataHolder.activeVehicles.forEach { vehicle ->
-                        val vehicleRoute = vehicle.currentRoute
-                        val vehiclePosition = vehicle.lastVisitedVertex
-                        vehicle.currentRoute = dataHolder.graph.calculateShortestRoute(
-                            vehiclePosition,
-                            vehicleRoute.last(),
-                            vehicle.height
-                        )
-                    }
                 }
             }
         }
         // Remove completed events from the list
         events.removeIf { event -> event.duration == 0 }
+    }
+
+    private fun rerouteVehicles() {
+        dataHolder.activeVehicles.forEach { vehicle ->
+            val vehicleRoute = vehicle.currentRoute
+            val vehiclePosition = vehicle.lastVisitedVertex
+            vehicle.currentRoute = dataHolder.graph.calculateShortestRoute(
+                vehiclePosition,
+                vehicleRoute.last(),
+                vehicle.height
+            )
+        }
     }
 }
