11e005711cdffcaf7bcdfd9949278d888e613dad
Umama <umri00001@stud.uni-saarland.de>
2023-10-05 17:44:34 +0200

    fixed so it aligns with new implementation

--- src/test/kotlin/graphtests/GraphComplexClassTests.kt
+++ src/test/kotlin/graphtests/GraphComplexClassTests.kt
@@ -341,7 +341,7 @@ class GraphComplexClassTests {
 
     @Test
     fun findClosestBaseByProximityTest1() {
-        val checkingList = graph.findClosestBasesByProximity(emergency, hospital1, listOfBases, baseToVertex)
+        val checkingList = graph.findClosestBasesByProximity(EmergencyType.MEDICAL, hospital1, listOfBases, baseToVertex)
         val expectedList = listOf(hospital2, hospital4, hospital5, hospital3)
         assert(checkingList == expectedList)
     }
@@ -350,7 +350,7 @@ class GraphComplexClassTests {
     fun findClosestBaseByProximityTest2() {
         // With events
         graph.applyGraphEvent(constructionEventForProximity)
-        val checkingList = graph.findClosestBasesByProximity(emergency, hospital1, listOfBases, baseToVertex)
+        val checkingList = graph.findClosestBasesByProximity(EmergencyType.MEDICAL, hospital1, listOfBases, baseToVertex)
         val expectedList = listOf(hospital2, hospital4, hospital5, hospital3)
         assert(checkingList == expectedList)
     }
