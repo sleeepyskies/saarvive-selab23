cfaa727ed8dfbb8ac4a01d6ac4642f6d18b62a2a
Umama <umri00001@stud.uni-saarland.de>
2023-10-05 11:55:09 +0200

    modified so it entirely depends on allocation helper

--- src/main/kotlin/de/unisaarland/cs/se/selab/phases/AllocationPhase.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/phases/AllocationPhase.kt
@@ -4,11 +4,6 @@ import de.unisaarland.cs.se.selab.dataClasses.Request
 import de.unisaarland.cs.se.selab.dataClasses.bases.Base
 import de.unisaarland.cs.se.selab.dataClasses.emergencies.Emergency
 import de.unisaarland.cs.se.selab.dataClasses.emergencies.EmergencyStatus
-import de.unisaarland.cs.se.selab.dataClasses.vehicles.Ambulance
-import de.unisaarland.cs.se.selab.dataClasses.vehicles.CapacityType
-import de.unisaarland.cs.se.selab.dataClasses.vehicles.FireTruckWater
-import de.unisaarland.cs.se.selab.dataClasses.vehicles.FireTruckWithLadder
-import de.unisaarland.cs.se.selab.dataClasses.vehicles.PoliceCar
 import de.unisaarland.cs.se.selab.dataClasses.vehicles.Vehicle
 import de.unisaarland.cs.se.selab.dataClasses.vehicles.VehicleStatus
 import de.unisaarland.cs.se.selab.simulation.DataHolder
@@ -30,12 +25,12 @@ class AllocationPhase(private val dataHolder: DataHolder) : Phase {
             ) {
                 val base = dataHolder.emergencyToBase[emergency.id] ?: Base(1, 1, 1, mutableListOf())
 
-                val assignableVehicle = getAssignableAssets(base, emergency)
-                assignBasedOnCapacity(assignableVehicle, emergency)
+                val assignableVehicles = allocationHelper.getAssignableAssets(base, emergency)
+                sortAndAssign(assignableVehicles, emergency)
 
                 if (emergency.requiredVehicles.isNotEmpty()) {
                     val reallocatableVehicles = getReallocatableVehicles(base, emergency)
-                    assignBasedOnCapacity(reallocatableVehicles, emergency)
+                    sortAndAssign(reallocatableVehicles, emergency)
                 }
 
                 if (emergency.requiredVehicles.isNotEmpty()) {
@@ -45,62 +40,70 @@ class AllocationPhase(private val dataHolder: DataHolder) : Phase {
         }
     }
 
-    /**
-     * Returns all vehicles that are in a base that are of the correct vehicle type for an emergency
-     */
-    fun getAssignableAssets(base: Base, emergency: Emergency): List<Vehicle> {
-        val requiredVehicles = emergency.requiredVehicles
-        val vehicles = base.vehicles
+    private fun sortAndAssign(vehicles: List<Vehicle>, emergency: Emergency) {
+        val normalVehicles = allocationHelper.getNormalVehicles(vehicles)
+        val specialVehicles = allocationHelper.getSpecialVehicles(vehicles)
 
-        return vehicles
-            .filter { it.vehicleStatus == VehicleStatus.IN_BASE }
-            .filter { it.isAvailable }
-            .filter { it.vehicleType in requiredVehicles }
+        allocationHelper.assignWithoutCapacity(normalVehicles, emergency)
+        allocationHelper.assignBasedOnCapacity(specialVehicles, emergency)
     }
 
-    /**
-     * Returns the current amount of capacity a vehicle has
-     */
-    private fun getVehicleCapacity(vehicle: Vehicle): Pair<CapacityType, Int> {
-        return when (vehicle) {
-            is PoliceCar -> Pair(CapacityType.CRIMINAL, vehicle.maxCriminalCapacity - vehicle.currentCriminalCapcity)
-            is FireTruckWater -> Pair(CapacityType.WATER, vehicle.maxWaterCapacity - vehicle.currentWaterCapacity)
-            is Ambulance -> Pair(CapacityType.PATIENT, if (vehicle.hasPatient) 0 else 1)
-            is FireTruckWithLadder -> Pair(CapacityType.LADDER_LENGTH, vehicle.ladderLength)
-            else -> Pair(CapacityType.NONE, 0)
-        }
-    }
-
-    /** Assigns vehicles to an emergency based on their capacity
-     *
-     * @param assets The list of vehicles that can be assigned to the emergency from the assigned base
-     * @param emergency The emergency that needs to be assigned vehicles
-     */
-    private fun assignBasedOnCapacity(assets: List<Vehicle>, emergency: Emergency) {
-        val requiredVehicle = emergency.requiredVehicles
-        for (asset in assets) if (requiredVehicle.isNotEmpty()) {
-            // check if vehicle has the required capacity
-            val vehicleCapacity = getVehicleCapacity(asset)
-            // allocationHelper.assignBasedOnCapacity(asset, vehicleCapacity, emergency)
-            checkAndAssign(vehicleCapacity, asset, emergency)
-            // val arrival = getTimeToArrive(asset, emergency)
-            // Log.displayAssetAllocation(asset.id, emergency.id, arrival)
-        } else {
-            break
-        }
-        emergency.emergencyStatus = EmergencyStatus.ONGOING
-    }
-
-    /**
-     *  check if a vehicle fits the requirements for an emergency and assigns accordingly
-     */
-    fun checkAndAssign(
-        vehicleCapacity: Pair<CapacityType, Int>,
-        asset: Vehicle,
-        emergency: Emergency
-    ) {
-        allocationHelper.assignBasedOnCapacity(asset, vehicleCapacity, emergency)
-    }
+//    /**
+//     * Returns all vehicles that are in a base that are of the correct vehicle type for an emergency
+//     */
+//    fun getAssignableAssets(base: Base, emergency: Emergency): List<Vehicle> {
+//        val requiredVehicles = emergency.requiredVehicles
+//        val vehicles = base.vehicles
+//
+//        return vehicles
+//            .filter { it.vehicleStatus == VehicleStatus.IN_BASE }
+//            .filter { it.isAvailable }
+//            .filter { it.vehicleType in requiredVehicles }
+//    }
+//
+//    /**
+//     * Returns the current amount of capacity a vehicle has
+//     */
+//    private fun getVehicleCapacity(vehicle: Vehicle): Pair<CapacityType, Int> {
+//        return when (vehicle) {
+//            is PoliceCar -> Pair(CapacityType.CRIMINAL, vehicle.maxCriminalCapacity - vehicle.currentCriminalCapcity)
+//            is FireTruckWater -> Pair(CapacityType.WATER, vehicle.maxWaterCapacity - vehicle.currentWaterCapacity)
+//            is Ambulance -> Pair(CapacityType.PATIENT, if (vehicle.hasPatient) 0 else 1)
+//            is FireTruckWithLadder -> Pair(CapacityType.LADDER_LENGTH, vehicle.ladderLength)
+//            else -> Pair(CapacityType.NONE, 0)
+//        }
+//    }
+//
+//    /** Assigns vehicles to an emergency based on their capacity
+//     *
+//     * @param assets The list of vehicles that can be assigned to the emergency from the assigned base
+//     * @param emergency The emergency that needs to be assigned vehicles
+//     */
+//    private fun assignBasedOnCapacity(assets: List<Vehicle>, emergency: Emergency) {
+//        val requiredVehicle = emergency.requiredVehicles
+//        for (asset in assets) if (requiredVehicle.isNotEmpty()) {
+//            // check if vehicle has the required capacity
+//            val vehicleCapacity = getVehicleCapacity(asset)
+//            // allocationHelper.assignBasedOnCapacity(asset, vehicleCapacity, emergency)
+//            checkAndAssign(vehicleCapacity, asset, emergency)
+//            // val arrival = getTimeToArrive(asset, emergency)
+//            // Log.displayAssetAllocation(asset.id, emergency.id, arrival)
+//        } else {
+//            break
+//        }
+//        emergency.emergencyStatus = EmergencyStatus.ONGOING
+//    }
+//
+//    /**
+//     *  check if a vehicle fits the requirements for an emergency and assigns accordingly
+//     */
+//    fun checkAndAssign(
+//        vehicleCapacity: Pair<CapacityType, Int>,
+//        asset: Vehicle,
+//        emergency: Emergency
+//    ) {
+//        allocationHelper.assignBasedOnCapacity(asset, emergency)
+//    }
 
     private fun getReallocatableVehicles(base: Base, emergency: Emergency): List<Vehicle> {
         val neededTypes = emergency.requiredVehicles.keys
