4bd44e2089e5bf7052a841aa8b7e8ed5df0a20bf
Iryna Kmet <irkm00001@stud.uni-saarland.de>
2023-10-06 22:02:17 +0200

    sys test reg

--- src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/SystemTestRegistration.kt
+++ src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/SystemTestRegistration.kt
@@ -48,6 +48,19 @@ object SystemTestRegistration {
         manager.registerTest(FATHERvalidScenario())
         manager.registerTest(InvalidScenario1())
         manager.registerTest(FireTrucksRefillTest())
+        manager.registerTest(AllocateFireEmergencyOneTest1())
+        manager.registerTest(AllocateFireEmergencyOneTest2())
+        manager.registerTest(AllocateFireEmergencyOneTest3())
+        manager.registerTest(TravelMultipleVerticesTest())
+        manager.registerTest(ValidScenario1())
+        manager.registerTest(ValidScenario2())
+        manager.registerTest(ValidScenario3())
+        manager.registerTest(BROvalidScenario())
+        manager.registerTest(DADvalidScenario())
+        manager.registerTest(SISvalidScenario())
+        manager.registerTest(InvalidScenario1())
+        manager.registerTest(MedicalSeverity3())
+        manager.registerTest(InvalidScenario1())
     }
 
     fun registerSystemTestsMutantValidation(manager: SystemTestManager) {
@@ -127,7 +140,6 @@ object SystemTestRegistration {
         manager.registerTest(BROvalidScenario())
         manager.registerTest(DADvalidScenario())
         manager.registerTest(SISvalidScenario())
-        manager.registerTest(InvalidScenario1())
         manager.registerTest(MedicalSeverity3())
     }
 }
