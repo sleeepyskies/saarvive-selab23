f497b0b286c49550264686dc77ce8072898cfc99
Umama <umri00001@stud.uni-saarland.de>
2023-10-06 23:11:40 +0200

    same system registration

--- src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/SystemTestRegistration.kt
+++ src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/SystemTestRegistration.kt
@@ -47,6 +47,7 @@ object SystemTestRegistration {
 
     fun registerSystemTestsReferenceImpl(manager: SystemTestManager) {
         manager.registerTest(ComplexScenario1Test())
+        manager.registerTest(SISvalidScenario())
         manager.registerTest(FATHERvalidScenario())
         manager.registerTest(InvalidScenario1())
         manager.registerTest(FireTrucksRefillTest())
@@ -142,6 +143,9 @@ object SystemTestRegistration {
         manager.registerTest(ValidScenario3())
         manager.registerTest(BROvalidScenario())
         manager.registerTest(DADvalidScenario())
+        manager.registerTest(AllocateFireEmergencyOneTest3())
+        manager.registerTest(MedicalSeverity3())
+        manager.registerTest(FATHERvalidScenario())
         manager.registerTest(SISvalidScenario())
         manager.registerTest(MedicalSeverity3())
     }
