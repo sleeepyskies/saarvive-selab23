5b6a7e86cafa8c3305b179487ce3310ffbd1d0b0
ykamila <ykamila23@gmail.com>
2023-10-06 11:52:00 +0200

    updated scenario 2

--- src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/mysystemtest/validtests/scenarios/ValidScenario2.kt
+++ src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/mysystemtest/validtests/scenarios/ValidScenario2.kt
@@ -10,7 +10,7 @@ class ValidScenario2 : SystemTest() {
     override val maxTicks = 50
 
     override suspend fun run() {
-        assertNextLine("Initialization Info: validScenario2_map.dot successfully parsed and validated")
+        assertNextLine("Initialization Info: decentGraphSimTests.dot successfully parsed and validated")
         assertNextLine("Initialization Info: validScenario2_bases.json successfully parsed and validated")
         assertNextLine("Initialization Info: validScenario2_simulation.json successfully parsed and validated")
         // start of simulation
@@ -41,43 +41,57 @@ class ValidScenario2 : SystemTest() {
         assertNextLine("Asset Allocation: 1 allocated to 0; 1 ticks to arrive.")
         assertNextLine("Asset Allocation: 18 allocated to 0; 1 ticks to arrive.")
         assertNextLine("Event Triggered: 0 triggered.") // rush hour affects the ambulance 0 for event 1
-        assertNextLine("Assets Rerouted: 14") //
+        assertNextLine("Assets Rerouted: 1") //
         assertNextLine("Simulation Tick: 4")
-        assertNextLine("Event Triggered: 1 triggered.")
-        assertNextLine("Assets Rerouted: 16") // all the fire, ambulance and 1 extra ambulance
-        assertNextLine("Simulation Tick: 5")
+        assertNextLine("Asset Arrival: 0 arrived at 9.")
         assertNextLine("Asset Arrival: 1 arrived at 8.")
         assertNextLine("Asset Arrival: 18 arrived at 8.")
+        assertNextLine("Emergency Handling Start: 1 handling started.")
+        assertNextLine("Event Triggered: 1 triggered.")
+        assertNextLine("Assets Rerouted: 10") // all the fire, ambulance and 1 extra ambulance
+        assertNextLine("Simulation Tick: 5")
         assertNextLine("Simulation Tick: 6")
+        assertNextLine("Asset Arrival: 6 arrived at 6.")
+        assertNextLine("Asset Arrival: 7 arrived at 6.")
+        assertNextLine("Asset Arrival: 8 arrived at 6.")
+        assertNextLine("Asset Arrival: 9 arrived at 6.")
+        assertNextLine("Asset Arrival: 10 arrived at 6.")
+        assertNextLine("Asset Arrival: 11 arrived at 6.")
+        assertNextLine("Asset Arrival: 12 arrived at 6.")
+        assertNextLine("Asset Arrival: 13 arrived at 6.")
+        assertNextLine("Asset Arrival: 15 arrived at 6.")
+        assertNextLine("Asset Arrival: 16 arrived at 6.")
         assertNextLine("Event Ended: 1 ended.") // road closure event
-        assertNextLine("Assets Rerouted: 12")
         // vehicles arrive at tick 6
         assertNextLine("Simulation Tick: 7")
         assertNextLine("Simulation Tick: 8")
-//        assertNextLine("Event Ended: 0 ended.") // rush hour event
-//        assertNextLine("Assets Rerouted: 12") // more are rerouted
-//        assertNextLine("Simulation Tick: 9")
-//        assertNextLine("Asset Arrival: 0 arrived at 9.")
-//        assertNextLine("Asset Arrival: 6 arrived at 6.")
-//        assertNextLine("Asset Arrival: 7 arrived at 6.")
-//        assertNextLine("Asset Arrival: 8 arrived at 6.")
-//        assertNextLine("Asset Arrival: 9 arrived at 6.")
-//        assertNextLine("Asset Arrival: 10 arrived at 6.")
-//        assertNextLine("Asset Arrival: 11 arrived at 6.")
-//        assertNextLine("Asset Arrival: 12 arrived at 6.")
-//        assertNextLine("Asset Arrival: 13 arrived at 6.")
-//        assertNextLine("Asset Arrival: 15 arrived at 6.")
-//        assertNextLine("Asset Arrival: 16 arrived at 6.")
-//        assertNextLine("Emergency Handling Start: 1 handling started.")
-//        assertNextLine("Simulation Tick: 10")
-//        assertNextLine("Simulation Tick: 11")
-//        assertNextLine("Simulation Tick: 12")
-//        assertNextLine("Emergency Handling Start: 0 handling started.")
-//        assertNextLine("Simulation Tick: 13")
-//        assertNextLine("Simulation Tick: 14")
-//        assertNextLine("Emergency Resolved: 1 resolved.")
-//        assertNextLine("Simulation Tick: 15")
-//        assertNextLine("Simulation Tick: 16")
+        assertNextLine("Event Ended: 0 ended.") // rush hour event
+        assertNextLine("Assets Rerouted: 12") // more are rerouted
+        assertNextLine("Simulation Tick: 9")
+        assertNextLine("Asset Arrival: 2 arrived at 6.")
+        assertNextLine("Emergency Resolved: 1 resolved.")
+        assertNextLine("Emergency Handling Start: 0 handling started.")
+        assertNextLine("Simulation Tick: 10")
+        assertNextLine("Simulation Tick: 11")
+        assertNextLine("Simulation Tick: 12")
+        assertNextLine("Asset Arrival: 0 arrived at 5.")
+        assertNextLine("Simulation Tick: 13")
+        assertNextLine("Simulation Tick: 14")
+        assertNextLine("Simulation Tick: 15")
+        assertNextLine("Simulation Tick: 16")
+        assertNextLine("Simulation Tick: 17")
+        assertNextLine("Simulation Tick: 18")
+        assertNextLine("Simulation Tick: 19")
+        assertNextLine("Emergency Resolved: 1 resolved.")
+        assertNextLine("Simulation End")
+        // Statistics
+        assertNextLine("Simulation Statistics: 0 assets rerouted.")
+        assertNextLine("Simulation Statistics: 2 received emergencies.")
+        assertNextLine("Simulation Statistics: 0 ongoing emergencies.")
+        assertNextLine("Simulation Statistics: 0 failed emergencies.")
+        assertNextLine("Simulation Statistics: 2 resolved emergencies.")
+        // end of file is reached
+        assertEnd()
         // emergency 1 ends
     }
 }
