530b066f86089a9b2157857a140429d211c108d3
Iryna Kmet <irkm00001@stud.uni-saarland.de>
2023-10-06 14:35:24 +0200

    Fixes

--- src/main/kotlin/de/unisaarland/cs/se/selab/phases/RequestPhase.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/phases/RequestPhase.kt
@@ -25,8 +25,8 @@ class RequestPhase(private val dataHolder: DataHolder) : Phase {
             val assignableVehicles = allocationHelper.getAssignableAssets(base, emergency)
 
             // quick fix
-            val sortedAssignableAssets = assignableVehicles.sortedBy { it.id }
-            for (vehicle in sortedAssignableAssets) {
+            val assignableVehiclesSorted = assignableVehicles.sortedBy { it.id }
+            for (vehicle in assignableVehiclesSorted) {
                 if (allocationHelper.isNormalVehicle(vehicle)) {
                     allocationHelper.assignWithoutCapacity(vehicle, emergency, false)
                 } else { allocationHelper.assignBasedOnCapacity(vehicle, emergency, false) }
