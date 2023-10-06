386c62df56b215396f35e2c04fe5d019e1e35b93
Iryna Kmet <irkm00001@stud.uni-saarland.de>
2023-10-06 18:09:43 +0200

    cleaned

--- src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/SystemTestRegistration.kt
+++ src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/SystemTestRegistration.kt
@@ -97,78 +97,10 @@ object SystemTestRegistration {
     }
 
     fun registerSystemTestsReferenceImpl(manager: SystemTestManager) {
-        // manager.registerTest(ExampleTest())
-        /* manager.registerTest(TwoRoadsSameLocationTest())
-         manager.registerTest(TwoRoadsSameNameTest())
-         manager.registerTest(NonExistentVertexTest())
-         manager.registerTest(ZeroHeightTest())
-         manager.registerTest(NegativeVertexTest())
-         manager.registerTest(NoMainStreetTest())
-         manager.registerTest(NonUniqueVertexTest())
-         manager.registerTest(NoSideStreetTest())
-         manager.registerTest(RoadHas2SameVerticesTest())
-         manager.registerTest(SameVertexDifferentVillagesTest())
-         manager.registerTest(VertexWithoutRoadConnectionTest())
-         manager.registerTest(VillageAndCountyNameSameTest())
-         manager.registerTest(WeightIsZeroTest())
-         manager.registerTest(WrongTunnelHeightTest())
-         manager.registerTest(WrongNumericIDTest())
-         manager.registerTest(MissingAttributeTest())
-         manager.registerTest(NoDigraphNameTest())
-         manager.registerTest(NoVerticesTest())
-         manager.registerTest(SplittedNameTest())
-         manager.registerTest(MissingTerminalTest())
-         manager.registerTest(NotTheEndTest())
-         manager.registerTest(ValidSmallMapSpacesKingdomTest())
-         manager.registerTest(InvalidStringIDTest())
-         manager.registerTest(HasOnlyTwoVerticesTest())
-         manager.registerTest(HasTwoVerticesAndARoadTest())
-         manager.registerTest(HasOnlyRoadsTest())*/
-        /*        manager.registerTest(HasReversedDataTest())
-                manager.registerTest(VillagesWithSameNameTest())
-                manager.registerTest(CountySameRoadNameTest())
-                manager.registerTest(CorrectEdgesToVerticesTest())*/
-        // asset parser tests
-        /*        manager.registerTest(NoVehiclesInBase())
-                manager.registerTest(NotEnoughBases())
-                manager.registerTest(SameBaseLocation())
-                manager.registerTest(BaseOnNothing())
-                manager.registerTest(WhatBaseTest())
-                manager.registerTest(BaseWrongAssetsTest())*/
-        // manager.registerTest(PuppyHeavenTest())
-        /*     manager.registerTest(AnAppleADayTest())
-             manager.registerTest(DoctorDawgTest())
-             manager.registerTest(FireDoctorsTest())
-             manager.registerTest(SameIDAssetsTest())
-             manager.registerTest(SameIDBaseTest())
-             manager.registerTest(NotEnoughWaterTest())
-             manager.registerTest(WrongWaterTest())
-             manager.registerTest(BackseatDriversTest())
-             manager.registerTest(LongLadderTest())
-             manager.registerTest(TallCarTest())
-             manager.registerTest(LadderBikeTest())
-             manager.registerTest(NoCriminalsTest())
-             manager.registerTest(TooManyCriminals())
-             manager.registerTest(WrongCarWrongStuffTest())
-             // manager.registerTest(TwoBasesTest())
-             // simulation tests
-             manager.registerTest(EmptySimulationTest())
-             manager.registerTest(WrongEmergenciesTest())
-             manager.registerTest(WrongEventsTest())*/
-        manager.registerTest(NoRoadBetween())
-        // phase tests
-        // manager.registerTest(AllocateFireEmergencyOneTest1())
-        // manager.registerTest(AllocateFireEmergencyOneTest2())
         manager.registerTest(AllocateFireEmergencyOneTest3())
-        manager.registerTest(TravelMultipleVerticesTest())
-        // Valid scenarios
-        // manager.registerTest(ValidScenario1())
-        manager.registerTest(ValidScenario2())
-        // manager.registerTest(ValidScenario3())
         manager.registerTest(ComplexScenario1Test())
-        // manager.registerTest(BROvalidScenario())
         manager.registerTest(SISvalidScenario())
-        manager.registerTest(DADvalidScenario())
+
     }
 
     fun registerSystemTestsMutantValidation(manager: SystemTestManager) {
@@ -180,6 +112,7 @@ object SystemTestRegistration {
         manager.registerTest(NoMainStreetTest())
         manager.registerTest(NonUniqueVertexTest())
         manager.registerTest(NoSideStreetTest())
+        manager.registerTest(NoRoadBetween())
         manager.registerTest(RoadHas2SameVerticesTest())
         manager.registerTest(SameVertexDifferentVillagesTest())
         manager.registerTest(VertexWithoutRoadConnectionTest())
@@ -201,7 +134,6 @@ object SystemTestRegistration {
         manager.registerTest(VillagesWithSameNameTest())
         manager.registerTest(CountySameRoadNameTest())
         manager.registerTest(CorrectEdgesToVerticesTest())
-        manager.registerTest(NoRoadBetween())
 
         // asset parser tests
         manager.registerTest(NoVehiclesInBase())
@@ -244,5 +176,7 @@ object SystemTestRegistration {
         manager.registerTest(ValidScenario2())
         manager.registerTest(ValidScenario3())
         manager.registerTest(BROvalidScenario())
+        manager.registerTest(DADvalidScenario())
+
     }
 }
